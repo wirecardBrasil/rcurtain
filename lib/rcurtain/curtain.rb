6297c2ee99568160a0efe2d43ec3e8a00e55e4c7
gabrielqueiroz <gabriel.fatec@hotmail.com>
2016-05-24 17:43:07 -0300

    Updated curtain and feature

--- lib/rcurtain/curtain.rb
+++ lib/rcurtain/curtain.rb
@@ -1,36 +1,40 @@
 require "redis"
+require "rcurtain/feature"
 
 module Rcurtain
   class Curtain
 
     attr_reader :redis
 
-    def initialize(url)
+    def initialize (url)
       @redis = Redis.new(:url => url)
     end
 
-    def opened?(feature, user: nil?)
+    def opened? (feature, users= nil)
       feat = get_feature(feature)
 
-      (compare_percentage(feat.percentage) || feat.users.include? user)
+      if users
+        (compare_percentage?(feat.percentage)) || (feat.users.any? { |user| users.include?(user)})
+      else
+        compare_percentage?(feat.percentage)
+      end
     end
 
     def get_feature (name)
-      percentage = @redis.get("feature:#{name}:percentage")
+      percentage = redis.get("feature:#{name}:percentage")
       percentage = 0 if percentage.nil?
-  ​
-      users = @redis.smembers("feature:#{name}:users")
-  ​
+
+      users = redis.smembers("feature:#{name}:users")
+      users = [''] if users.nil?
+
       feature = Feature.new(name, percentage, users)
-  ​
+
       feature
     end
 
     def compare_percentage? (percentage)
-      random = Random.new
-      random.rand(1..100)
-
-      (random <= percentage)
+      rnd = Random.new
+      rnd.rand(1..100) <= percentage
     end
 
   end
--- lib/rcurtain/feature.rb
+++ lib/rcurtain/feature.rb
@@ -2,7 +2,7 @@ class Feature
 
   attr_accessor :name, :percentage, :users
 
-  def initalize (name:, percentage:, users:)
+  def initialize (name, percentage, users)
     @name = name
     @percentage = percentage
     @users = users
