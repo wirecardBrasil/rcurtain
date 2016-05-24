65d9f2d4e47e31a1d4041958e216b050006806f7
gabrielqueiroz <gabriel.fatec@hotmail.com>
2016-05-24 17:43:29 -0300

    Added stub to tests

--- spec/lib/curtain_spec.rb
+++ spec/lib/curtain_spec.rb
@@ -1,30 +1,62 @@
 require "spec_helper"
 
 describe "::Rcurtain" do
+
   context "is opened" do
+
     context "when 100%" do
+      before do
+        allow_any_instance_of(Redis).to receive(:get).and_return(100)
+        allow_any_instance_of(Redis).to receive(:smembers).and_return(nil)
+      end
+
       it { expect(Rcurtain.new("redis://:p4ssw0rd@10.0.1.1:6380/15").opened? "feature").to be true }
     end
 
     context "when user exists" do
-      it { expect(Rcurtain.new("redis://:p4ssw0rd@10.0.1.1:6380/15").opened? "feature").to be true }
+      before do
+        allow_any_instance_of(Redis).to receive(:get).and_return(0)
+        allow_any_instance_of(Redis).to receive(:smembers).and_return(['MPA-123'])
+      end
+
+      it { expect(Rcurtain.new("redis://:p4ssw0rd@10.0.1.1:6380/15").opened?("feature", ['MPA-123'])).to be true }
     end
   end
 
   context "is closed" do
     context "when 0%" do
+      before do
+        allow_any_instance_of(Redis).to receive(:get).and_return(0)
+        allow_any_instance_of(Redis).to receive(:smembers).and_return(nil)
+      end
+
       it { expect(Rcurtain.new("redis://:p4ssw0rd@10.0.1.1:6380/15").opened? "feature").to be false }
     end
 
     context "when user does not exists" do
-      it { expect(Rcurtain.new("redis://:p4ssw0rd@10.0.1.1:6380/15").opened? "feature").to be false }
+      before do
+        allow_any_instance_of(Redis).to receive(:get).and_return(0)
+        allow_any_instance_of(Redis).to receive(:smembers).and_return(nil)
+      end
+
+      it { expect(Rcurtain.new("redis://:p4ssw0rd@10.0.1.1:6380/15").opened?("feature", ['MPA-123'])).to be false }
     end
 
     context "when nothing was found on redis" do
+      before do
+        allow_any_instance_of(Redis).to receive(:get).and_return(nil)
+        allow_any_instance_of(Redis).to receive(:smembers).and_return(nil)
+      end
+
       it { expect(Rcurtain.new("redis://:p4ssw0rd@10.0.1.1:6380/15").opened? "feature_not_exists").to be false }
     end
 
     context "when failed connection" do
+      before do
+        allow_any_instance_of(Redis).to receive(:get).and_return(nil)
+        allow_any_instance_of(Redis).to receive(:smembers).and_return(nil)
+      end
+      
       it { expect(Rcurtain.new("redis://:p4ssw0rd@10.0.1.1:6380/15").opened? "feature_not_exists").to be false }
     end
   end
